76eb08d8c9a77c4a387dd1abcae73910583be15f
RichardBongani <phalarichard1@gmail.com>
2020-06-05 23:45:08 +0200

    adding quizzes

--- queries.sql
+++ queries.sql
@@ -43,3 +43,48 @@ SELECT name, population/area
   FROM world
  WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
 */
+
+QUIZZ 2
+
+/****QUESTION_1*****/
+
+/*
+SELECT name
+  FROM world
+ WHERE name LIKE 'U%'
+*/
+
+/****QUESTION_2*****/
+/*
+SELECT population
+  FROM world
+ WHERE name = 'United Kingdom'
+*/
+
+/****QUESTION_3*****/
+/*
+'name' should be name
+*/
+
+/****QUESTION_4*****/
+/*
+Nauru    990
+*/
+
+/****QUESTION_5*****/
+/*
+SELECT name, population
+  FROM world
+ WHERE continent IN ('Europe', 'Asia')
+*/
+
+/****QUESTION_6*****/
+/*
+SELECT name FROM world
+ WHERE name IN ('Cuba', 'Togo')
+*/
+
+/****QUESTION_7*****/
+/*
+Brazil & Colombia
+*/
