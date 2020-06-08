c41d55b573f0cd671a2a8a8c120695627900c7eb
RichardBongani <phalarichard1@gmail.com>
2020-06-08 18:32:47 +0200

    adding sql quizzes

--- queries.sql
+++ queries.sql
@@ -189,3 +189,215 @@ Bangladesh
 India
 Pakistan
 */
+
+QUIZZ 5
+
+/****QUESTION_1*****/
+
+/* SELECT SUM(population) FROM bbc WHERE region = 'Europe'*/
+
+/****QUESTION_2*****/
+
+/*
+ SELECT COUNT(name) FROM bbc WHERE population < 150000
+*/
+
+/****QUESTION_3*****/
+
+/*
+AVG(), COUNT(), MAX(), MIN(), SUM()
+*/
+
+/****QUESTION_4*****/
+
+/*
+No result due to invalid use of the WHERE function
+*/
+
+/****QUESTION_5*****/
+
+/*
+ SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark')
+*/
+
+/****QUESTION_6*****/
+
+/*
+SELECT region, SUM(population)/SUM(area) AS density FROM bbc GROUP BY region
+*/
+
+/****QUESTION_7*****/
+
+/*
+ SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc)
+*/
+
+/****QUESTION_7*****/
+
+/*
+Americas       732240
+Middle East    13403102
+South America  17740392
+South Asia     9437710
+*/
+
+
+QUIZZ 6
+
+/****QUESTION_1*****/
+
+/*
+ game  JOIN goal ON (id=matchid)
+*/
+
+/****QUESTION_2*****/
+
+/*
+ matchid, teamid, player, gtime, id, teamname, coach
+*/
+
+/****QUESTION_3*****/
+
+/*
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid
+*/
+
+/****QUESTION_4*****/
+
+/*
+DEN  9 June 2012
+GER  9 June 2012
+*/
+
+/****QUESTION_5*****/
+
+/*
+  SELECT DISTINCT player, teamid 
+   FROM game JOIN goal ON matchid = id 
+  WHERE stadium = 'National Stadium, Warsaw' 
+ AND (team1 = 'POL' OR team2 = 'POL')
+   AND teamid != 'POL'
+*/
+
+/****QUESTION_6*****/
+
+/*
+  SELECT DISTINCT player, teamid 
+   FROM game JOIN goal ON matchid = id 
+  WHERE stadium = 'National Stadium, Warsaw' 
+ AND (team1 = 'POL' OR team2 = 'POL')
+   AND teamid != 'POL'
+*/
+
+/****QUESTION_7*****/
+
+/*
+Netherlands   2
+Poland        2
+Republic of Ireland   1
+Ukraine    2
+*/
+
+
+QUIZZ 7
+
+/****QUESTION_1*****/
+
+/*
+SELECT name
+  FROM actor INNER JOIN movie ON actor.id = director
+ WHERE gross < budget
+*/
+
+/****QUESTION_2*****/
+
+/*
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid
+*/
+
+/****QUESTION_3*****/
+
+/*
+SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+ WHERE name LIKE 'John %'
+ GROUP BY name ORDER BY 2 DESC
+*/
+
+/****QUESTION_4*****/
+
+/*
+"Crocodile" Dundee
+Crocodile Dundee in Los Angeles
+Flipper
+Lightning Jack
+*/
+
+/****QUESTION_5*****/
+
+/*
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+*/
+
+/****QUESTION_6*****/
+
+/*
+- link the director column in movies with the primary key in actor
+- connect the primary keys of movie and actor via the casting table
+*/
+
+/****QUESTION_7*****/
+
+/*
+A Bronx Tale     1993
+Bang the Drum Slowly   1973
+Limitless    2011
+*/
+
+
+QUIZZ 8
+
+/****QUESTION_1*****/
+
+/*
+ SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+*/
+
+
+/****QUESTION_2*****/
+
+/*
+ SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+*/
+
+/****QUESTION_3*****/
+
+/*
+display 0 in result column for all teachers without department
+*/
+
+/****QUESTION_4*****/
+
+/*
+'four' for Throd
+*/
+
+/****QUESTION_5*****/
+
+/*
+hrivellComputing
+ThrodComputing
+SplintComputing
+SpiregrainOther
+CutflowerOther
+DeadyawnOther
+*/
